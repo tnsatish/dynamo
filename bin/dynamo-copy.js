445bdeec8c7861eb4f26fd3cdbc4094a03c6f07f
tnsatish <nagasatish.tiruveedula@xome.com>
2020-04-21 06:57:49 +0530

    Removed commented code and write statements

--- bin/dynamo-copy.js
+++ bin/dynamo-copy.js
@@ -62,19 +62,12 @@ destdynamo.describeTable(
         destStart = Date.now();
         destSecPerItem = Math.round(1000 / destQuota / ((argv.rate || 100) / 100));
         destDone = 0;
-	console.log("Data in describeTable event");
-	console.log(destQuota);
-	console.log(destStart);
-	console.log(destSecPerItem);
-	console.log(destDone);
 
 	var params = {
 		quota: data.Table.ProvisionedThroughput.WriteCapacityUnits,
 		msecPerItem: Math.round(1000 / destQuota / ((argv.rate || 100) / 100))
 	};
-
 	getSourceTable(params);
-
     }
 );
 
@@ -90,7 +83,6 @@ function getSourceTable(destParams) {
 	        if (data == null) {
 	            throw 'Table ' + argv.srctable + ' not found in DynamoDB';
 	        }
-	        //process.stdout.write("Data: " + JSON.stringify(data) + "\n");
 	        var srcParams = {
 	            TableName: argv.srctable,
 	            ReturnConsumedCapacity: 'NONE',
@@ -112,9 +104,7 @@ function search(srcParams, destParams) {
     var msecPerItem = Math.round(1000 / srcParams.Limit / ((argv.rate || 100) / 100));
     var method = srcParams.KeyConditions ? srcdynamo.query : srcdynamo.scan;
     var read = function(start, done, srcParams, destParams) {
-	    process.stdout.write("Start: " + start + "\n");
-	    process.stdout.write("Done: " + done + "\n");
-	    process.stdout.write("Params: " + JSON.stringify(srcParams) + "\n");
+	process.stdout.write("Start: " + start + ", Done: " + Done + "\n");
         method.call(
             srcdynamo,
             srcParams,
@@ -126,11 +116,7 @@ function search(srcParams, destParams) {
                     throw 'dynamo returned NULL instead of data';
                 }
 
-		    process.stdout.write("No.of Items: " + data.Items.length + "\n");
-		    process.stdout.write("Data: " + JSON.stringify(data) + "\n");
-
                 for (var idx = 0; idx < data.Items.length; idx++) {
-
 			destdynamo.putItem(
 	                    {
 	                        TableName: argv.desttable,
@@ -156,7 +142,6 @@ function search(srcParams, destParams) {
                 if (srcexpected > Date.now()) {
                     sleep(srcexpected - Date.now());
                 }
-		    process.stdout.write("Last EvaluatedKey: " + data.LastEaluatedKey + "\n");
                 if (data.LastEvaluatedKey) {
                     srcParams.ExclusiveStartKey = data.LastEvaluatedKey;
                     read(start, done, srcParams, destParams);
@@ -167,114 +152,3 @@ function search(srcParams, destParams) {
     read(Date.now(), 0, srcParams, destParams);
 };
 
-
-
-/*
-dynamo.describeTable(
-    {
-        TableName: argv.table
-    },
-    function (err, data) {
-        if (err != null) {
-            throw err;
-        }
-        if (data == null) {
-            throw 'Table ' + argv.table + ' not found in DynamoDB';
-        }
-        var quota = data.Table.ProvisionedThroughput.WriteCapacityUnits;
-        var start = Date.now();
-        var msecPerItem = Math.round(1000 / quota / ((argv.rate || 100) / 100));
-        var done = 0;
-        readline.createInterface(process.stdin, process.stdout).on(
-            'line',
-            function(line) {
-                dynamo.putItem(
-                    {
-                        TableName: argv.table,
-                        Item: JSON.parse(line)
-                    },
-                    function (err, data) {
-                        if (err) {
-                            console.log(err, err.stack);
-                            throw err;
-                        }
-                    }
-                );
-                ++done;
-                var expected = start + msecPerItem * done;
-                if (expected > Date.now()) {
-                    sleep(expected - Date.now());
-                }
-            }
-        );
-    }
-);
-
-function search(params) {
-    var msecPerItem = Math.round(1000 / params.Limit / ((argv.rate || 100) / 100));
-    var method = params.KeyConditions ? srcdynamo.query : srcdynamo.scan;
-    var read = function(start, done, params) {
-	    process.stdout.write("Start: " + start + "\n");
-	    process.stdout.write("Done: " + done + "\n");
-	    process.stdout.write("Params: " + JSON.stringify(params) + "\n");
-        method.call(
-            srcdynamo,
-            params,
-            function (err, data) {
-                if (err != null) {
-                    throw err;
-                }
-                if (data == null) {
-                    throw 'dynamo returned NULL instead of data';
-                }
-		    process.stdout.write("No.of Items: " + data.Items.length + "\n");
-		    process.stdout.write("Data: " + JSON.stringify(data) + "\n");
-                for (var idx = 0; idx < data.Items.length; idx++) {
-
-                    process.stdout.write(JSON.stringify(data.Items[idx]));
-                    process.stdout.write("\n");
-                }
-                var expected = start + msecPerItem * (done + data.Items.length);
-                if (expected > Date.now()) {
-                    sleep(expected - Date.now());
-                }
-		    process.stdout.write("Last EvaluatedKey: " + data.LastEaluatedKey + "\n");
-                if (data.LastEvaluatedKey) {
-                    params.ExclusiveStartKey = data.LastEvaluatedKey;
-                    read(start, done + data.Items.length, params);
-                }
-            }
-        );
-    };
-    read(Date.now(), 0, params);
-};
-
-srcdynamo.describeTable(
-    {
-        TableName: argv.table
-    },
-    function (err, data) {
-        if (err != null) {
-            throw err;
-        }
-        if (data == null) {
-            throw 'Table ' + argv.srctable + ' not found in DynamoDB';
-        }
-        process.stdout.write("Data: " + JSON.stringify(data) + "\n");
-        var params = {
-            TableName: argv.srctable,
-            ReturnConsumedCapacity: 'NONE',
-            Limit: data.Table.ProvisionedThroughput.ReadCapacityUnits
-        };
-        if (argv.index) {
-            params.IndexName = argv.index
-        }
-
-        if (argv.query) {
-            params.KeyConditions = JSON.parse(argv.query);
-        }
-        search(params);
-    }
-);
-
-*/
