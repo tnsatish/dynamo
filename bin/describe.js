00a4a6d1b344f4b57b47e6b8a284af8c5c6afa72
tnsatish <nagasatish.tiruveedula@xome.com>
2020-04-21 10:15:08 +0530

    Describe a table in dynamo

---
+++ bin/describe.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+/**
+ * Copyright 2013 Yegor Bugayenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var utils = require('../lib/utils');
+const fs = require('fs');
+
+var configData = fs.readFileSync('config.json');
+var config = JSON.parse(configData);
+
+var argv = utils.config({
+    demand: ['table'],
+    optional: ['key', 'secret', 'region', 'env'],
+    usage: 'Copy Dynamo DB tables from one AWS account to another AWS account\n' +
+           'Usage: describe --table table [--region us-east-1] [--key AK...AA] [--secret 7a...IG] [--env env]\n\n'
+});
+
+var dynamo = utils.dynamo({
+			table: argv.table,
+			key: argv.key || config.env[argv.env].aws_access_key_id,
+			secret: argv.secret || config.env[argv.env].aws_secret_access_key,
+			region: argv.region || config.env[argv.env].region
+		});
+
+dynamo.describeTable(
+    {
+        TableName: argv.table
+    },
+    function (err, data) {
+        if (err != null) {
+            throw err;
+        }
+        if (data == null) {
+            throw 'Table ' + argv.table + ' not found in DynamoDB';
+        }
+	console.log(data);
+    }
+);
+
